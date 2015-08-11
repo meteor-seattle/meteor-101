104f827e48e173f5f6a441516b3929419676af8f
Bryan Terrell <bryan.a.terrell@gmail.com>
2015-08-10 21:47:36 -0700

    Remove console logs

--- battle-cats-demo.js
+++ battle-cats-demo.js
@@ -3,16 +3,13 @@ if (Meteor.isClient) {
 
   Template.main.helpers({
     'cats': function () {
-      console.log('retrieving cats');
       
       if (!cat1) {
         cat1 = chooseRandomNumber(0, 13);
-        console.log('Loading cat1: ', cat1);
       }
       
       if (!cat2) {
         cat2 = chooseRandomNumber(0, 13, cat1);
-        console.log('Loading cat2: ', cat2);
       }
 
       return {cat1: cat1, cat2: cat2};
